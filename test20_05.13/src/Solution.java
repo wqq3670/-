d6243c6a600438689ef6390a6e7b487e20a80415
wqq3670 <1121093520@qq.com>
2020-05-14 18:33:37 +0800

    力扣 每日一题 05.13

--- test20_05.13/src/Solution.java
+++ test20_05.13/src/Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -18,20 +19,47 @@ class TreeNode {
 }
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while(size > 0) {
+                TreeNode cur = queue.poll();
+                list.add(cur.val);
+                size--;
+                System.out.println(cur.val);
+                if(cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            ret.add(list);
+        }
+        return ret;
+    }
+
+
+    public void levelOrder1(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         if(root != null) {
             queue.offer(root);
         }
-
         while(!queue.isEmpty()) {
             TreeNode cur = queue.poll();
-            System.out.println(cur.val);
-            if (cur.left != null) {
+            System.out.print(cur.val +" ");
+            if(cur.left != null) {
                 queue.offer(cur.left);
             }
-            if (cur.right != null) {
+            if(cur.right != null) {
                 queue.offer(cur.right);
             }
         }
-
+    }
 }

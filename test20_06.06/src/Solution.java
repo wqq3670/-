3dd1087293c1ef1eaffadd076e06cf7e9af1ef69
wqq3670 <1121093520@qq.com>
2020-06-06 23:57:05 +0800

    力扣 每日一题 06.06

---
+++ test20_06.06/src/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-06-06
+ * Time: 23:56
+ **/
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+
+
+    }
+}

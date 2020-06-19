5cf3c335b2246fb41a156688e696baf33eae68ae
wqq3670 <1121093520@qq.com>
2020-06-19 23:58:10 +0800

    力扣 每日一题 06.19

---
+++ test20_06.19/src/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-06-19
+ * Time: 23:57
+ **/
+public class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuffer sgood = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+        int n = sgood.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            if (Character.toLowerCase(sgood.charAt(left)) != Character.toLowerCase(sgood.charAt(right))) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+
+
+    }
+}

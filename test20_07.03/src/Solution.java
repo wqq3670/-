480966461e2390167695dacf3c08d75acc489791
wqq3670 <1121093520@qq.com>
2020-07-03 23:40:21 +0800

    力扣 每日一题 07.03

---
+++ test20_07.03/src/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-07-03
+ * Time: 23:39
+ **/
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        // 以升序数组的中间元素作为根节点 root。
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        // 递归的构建 root 的左子树与右子树。
+        root.left = dfs(nums, lo, mid - 1);
+        root.right = dfs(nums, mid + 1, hi);
+        return root;
+    }
+
+}

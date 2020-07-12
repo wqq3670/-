2c42bfdc0188a51e54d5f3664ae75ad01027b9ff
wqq3670 <1121093520@qq.com>
2020-07-12 23:52:00 +0800

    力扣 每日一题 07.12

---
+++ test20_07.12/src/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-07-12
+ * Time: 23:51
+ **/
+public class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int n = dungeon.length, m = dungeon[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; ++i) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        dp[n][m - 1] = dp[n - 1][m] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                int minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = Math.max(minn - dungeon[i][j], 1);
+            }
+        }
+        return dp[0][0];
+
+
+    }
+}
+}

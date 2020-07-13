77448613c2770a61e1f00aea04f485ab9ec56d6a
wqq3670 <1121093520@qq.com>
2020-07-13 23:49:18 +0800

    力扣 每日一题 07.13

---
+++ test20_07.13/src/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-07-13
+ * Time: 23:48
+ **/
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[Math.min(length1, length2)];
+        int index1 = 0, index2 = 0, index = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+
+
+    }
+}

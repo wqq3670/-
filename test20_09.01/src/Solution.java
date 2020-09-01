10137a02f1615369dbd5c2b88b5947e1cf395a7f
wqq3670 <1121093520@qq.com>
2020-09-01 23:52:15 +0800

    练习代码

---
+++ test20_09.01/src/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: dell
+ * Date: 2020-09-01
+ * Time: 23:51
+ **/
+public class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        return total(nums, 0, nums.length - 1, 1) >= 0;
+    }
+
+    public int total(int[] nums, int start, int end, int turn) {
+        if (start == end) {
+            return nums[start] * turn;
+        }
+        int scoreStart = nums[start] * turn + total(nums, start + 1, end, -turn);
+        int scoreEnd = nums[end] * turn + total(nums, start, end - 1, -turn);
+        return Math.max(scoreStart * turn, scoreEnd * turn) * turn;
+    }
+
+}
